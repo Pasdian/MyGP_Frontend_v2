78e41c77812ec9e376a9bb572e58aca51e046a10
miguelagza <miguelagzarate@gmail.com>
2025-07-01 16:44:32 -0600

    added uuid updateUser route

---
+++ app/api/users/updateUser/[uuid]/route.ts
@@ -0,0 +1,25 @@
+import { GPServer } from '@/axios-instance';
+import { logger } from '@/lib/logger';
+import { cookies } from 'next/headers';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function POST(req: NextRequest, { params }: { params: { uuid: string } }) {
+  const { uuid } = params;
+  const reqJSON = await req.json();
+  const session_token = (await cookies()).get('session_token')?.value;
+
+  logger.info(`POST /api/users/updateUser/${uuid} ${JSON.stringify(reqJSON)}`);
+
+  return await GPServer.post(`/api/users/updateUser/${uuid}`, reqJSON, {
+    headers: {
+      Authorization: `Bearer ${session_token}`,
+    },
+  })
+    .then((res) => {
+      return NextResponse.json(res.data, { status: res.status });
+    })
+    .catch((error) => {
+      logger.error(error);
+      return NextResponse.json({ message: error.response.data.message }, { status: error.status });
+    });
+}
