50f243dbbc4fafd5f512166d510a74967889d7f8
miguelagza <miguelagzarate@gmail.com>
2025-07-02 15:07:53 -0600

    Create User feature

--- app/admin-panel/usuarios/page.tsx
+++ app/admin-panel/usuarios/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import AdminPanelAddUserButton from '@/components/buttons/user/AdminPanelAddUserButton';
 import UsersDataTable from '@/components/datatables/UsersDataTable';
 import React from 'react';
 
@@ -7,6 +8,7 @@ export default function AdminPanelUsers() {
   return (
     <div>
       <h1 className="text-2xl font-bold tracking-tight mb-4">Administrar Usuarios</h1>
+      <AdminPanelAddUserButton />
       <UsersDataTable />
     </div>
   );
