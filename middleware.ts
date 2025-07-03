9d8b7581e6e02efedc3067933f6e4d04637e5fa3
miguelagza <miguelagzarate@gmail.com>
2025-07-02 21:08:18 -0600

    redirect to mygp

--- middleware.ts
+++ middleware.ts
@@ -3,6 +3,7 @@ import { NextRequest, NextResponse } from 'next/server';
 
 export default async function middleware(req: NextRequest) {
   const session_token = (await cookies()).get('session_token')?.value;
+
   if (req.nextUrl.pathname == '/') {
     return NextResponse.redirect(new URL('/login', req.url));
   }
@@ -12,7 +13,15 @@ export default async function middleware(req: NextRequest) {
   }
 
   if (session_token && req.nextUrl.pathname.startsWith('/login')) {
-    return NextResponse.redirect(new URL('/transbel/dashboard', req.url));
+    return NextResponse.redirect(new URL('/mygp/transbel/dashboard', req.url));
+  }
+
+  if (session_token && req.nextUrl.pathname.startsWith('/login')) {
+    return NextResponse.redirect(new URL('/mygp/transbel/dashboard', req.url));
+  }
+
+  if (session_token && req.nextUrl.pathname == '/mygp') {
+    return NextResponse.redirect(new URL('/mygp/dashboard', req.url));
   }
 
   return NextResponse.next();
