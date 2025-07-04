38b96414b952ec89f405da195a2e5b013c55d012
miguelagza <miguelagzarate@gmail.com>
2025-07-04 11:08:46 -0600

    added guia house columns

--- types/transbel/getDeliveries.ts
+++ types/transbel/getDeliveries.ts
@@ -1,6 +1,7 @@
 export type getDeliveries = {
   REFERENCIA: string | null;
   EE__GE: string | null;
+  GUIA_HOUSE: string | null;
   ENTREGA_TRANSPORTE_138: string | null;
   CE_138: string | null;
   ENTREGA_CDP_140: string | null;
