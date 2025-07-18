3ea0835bf624436a2f04bb63c160c61855db28bc
miguelagza <miguelagzarate@gmail.com>
2025-07-18 10:39:31 -0600

    optimized production build and redirect '/' to dashboard if user is logged in

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+'use client';
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
+
+export default function Root() {
+  const router = useRouter();
+  useEffect(() => {
+    router.push('/mygp/dashboard');
+  }, [router]);
+}
