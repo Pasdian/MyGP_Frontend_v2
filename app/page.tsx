64bdf6e986f6103669c3dcf97a05a4003614dece
miguelagza <miguelagzarate@gmail.com>
2025-07-21 10:15:37 -0600

    redirect to dashboard from /

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+'use client';
+import { useRouter } from 'next/navigation';
+import React from 'react';
+
+export default function Root() {
+  const router = useRouter();
+  React.useEffect(() => {
+    router.push('/mygp/dashboard');
+  }, []);
+}
