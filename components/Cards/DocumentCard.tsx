189491394a585e4baec5f9081d19c56963a86577
miguelagza <miguelagzarate@gmail.com>
2025-08-11 15:55:22 -0600

    dea feat: download and delete single files

--- components/Cards/DocumentCard.tsx
+++ components/Cards/DocumentCard.tsx
@@ -1,9 +1,13 @@
 import { Card } from '@/components/ui/card';
-import { DownloadIcon, Upload } from 'lucide-react';
+import { DownloadIcon, Trash2, Trash2Icon, TrashIcon, Upload } from 'lucide-react';
 import TailwindSpinner from '@/components/ui/TailwindSpinner';
 import React from 'react';
 import { IconUpload } from '@tabler/icons-react';
 import UploadFileDialog from '../Dialogs/UploadFileDialog';
+import { GPClient } from '@/lib/axiosUtils/axios-instance';
+import { useDEAStore } from '@/app/providers/dea-store-provider';
+import { toast } from 'sonner';
+import { mutate } from 'swr';
 
 export default function DocumentCard({
   title,
@@ -24,12 +28,65 @@ export default function DocumentCard({
   filterFn?: (item: string) => void;
   folder: string;
 }) {
+  const { reference, clientNumber: client, getFilesByReferenceKey } = useDEAStore((state) => state);
+
   const cardClassName = 'h-[240px] py-0 rounded-md';
   const cardHeaderClassName = 'h-full overflow-y-auto text-xs';
   const stickyClassName =
     'sticky top-0 bg-blue-500 p-2 text-white flex justify-between items-center';
 
   const [openDialog, setOpenDialog] = React.useState(false);
+
+  async function handleDownloadFile(file: string) {
+    try {
+      const res = await GPClient.post(
+        `/dea/downloadFile/${client}/${reference}/${folder}/${file}`,
+        null,
+        { responseType: 'blob' }
+      );
+
+      if (res.status !== 200) {
+        toast.error(`Error al descargar el archivo (${res.status})`);
+        return;
+      }
+
+      let downloadName = file;
+      const disposition = res.headers['content-disposition'];
+
+      if (disposition) {
+        const match = disposition.match(/filename="?(.+)"?/);
+        if (match?.[1]) downloadName = match[1];
+      }
+
+      const url = window.URL.createObjectURL(res.data);
+      const link = document.createElement('a');
+      link.href = url;
+      link.download = downloadName;
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+      window.URL.revokeObjectURL(url);
+    } catch {
+      toast.error('Error al descargar el archivo');
+    }
+  }
+
+  async function handleDeleteFile(file: string) {
+    try {
+      const res = await GPClient.post(`/dea/deleteFile/${client}/${reference}/${folder}/${file}`);
+
+      if (res.status !== 200) {
+        toast.error(`Error al eliminar el archivo (${res.status})`);
+        return;
+      }
+
+      toast.success(res.data.message);
+      mutate(getFilesByReferenceKey);
+    } catch {
+      toast.error('Error al descargar el archivo');
+    }
+  }
+
   return (
     <Card className={cardClassName}>
       <div className={cardHeaderClassName}>
@@ -53,19 +110,30 @@ export default function DocumentCard({
             ) : null}
           </div>
         </div>
-        <div className="p-2 break-words">
+        <div className="p-2">
           {files.filter(filterFn).map((item) => (
-            <p
+            <div
               key={item}
-              className={
-                item === activeFile
-                  ? 'bg-green-300 cursor-pointer mb-1'
-                  : 'cursor-pointer mb-1 even:bg-gray-200'
-              }
+              className={`flex justify-between p-1 items-center cursor-pointer mb-1 even:bg-gray-200 ${
+                item === activeFile ? 'bg-green-300' : ''
+              }`}
               onClick={() => onFileSelect(item)}
+              style={{ maxWidth: '100%' }} // asegurar ancho máximo si necesario
             >
-              {item}
-            </p>
+              <div className="max-w-[70%]">
+                <p className="break-words">{item}</p>
+              </div>
+              <div className="flex">
+                <DownloadIcon
+                  className="mr-2"
+                  size={20}
+                  onClick={() => {
+                    handleDownloadFile(item);
+                  }}
+                />
+                <Trash2Icon size={20} onClick={() => handleDeleteFile(item)} />
+              </div>
+            </div>
           ))}
         </div>
       </div>
