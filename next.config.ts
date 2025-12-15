5db5f62484c1f9934c11b671b9f9e1570fcd0b8d
Miguel Gonzalez <miguelagzarate@gmail.com>
2025-12-15 00:13:39 -0600

    test config

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,14 @@ import pkg from './package.json' assert { type: 'json' };
 
 const nextConfig = {
   experimental: {
-    serverActions: {
-      bodySizeLimit: '20mb',
-    },
+    serverActions: { bodySizeLimit: '20mb' },
   },
   env: {
-    NEXT_PUBLIC_RELEASE_VERSION: pkg.version, // bump only on big changes
+    NEXT_PUBLIC_RELEASE_VERSION: pkg.version,
+    NEXT_PUBLIC_PYTHON_API_KEY: process.env.NEXT_PUBLIC_PYTHON_API_KEY,
+    NEXT_PUBLIC_POSTHOG_KEY: process.env.NEXT_PUBLIC_POSTHOG_KEY,
+    NEXT_PUBLIC_POSTHOG_HOST: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+    NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
   },
   output: 'standalone',
 };
