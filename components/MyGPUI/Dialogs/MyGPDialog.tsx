71950d53c3f0e63c632dfcdd37af4081c4e97049
Miguel Gonzalez <miguelagzarate@gmail.com>
2026-03-27 17:34:03 -0600

    expand dialog based on content

--- components/MyGPUI/Dialogs/MyGPDialog.tsx
+++ components/MyGPUI/Dialogs/MyGPDialog.tsx
@@ -29,20 +29,22 @@ export function MyGPDialog({
 
       <DialogContent
         className="
-          top-0 left-0
-          h-[100dvh] max-h-[100dvh]
+          top-[50%] left-0
+          flex flex-col justify-start
+          h-auto max-h-[100dvh]
           w-screen max-w-none
-          translate-x-0 translate-y-0
+          translate-x-0 translate-y-[-50%]
           overflow-y-auto
           rounded-none border-0 p-4
           sm:top-[50%] sm:left-[50%]
+          sm:grid
           sm:h-auto sm:w-full
           sm:max-h-[80vh] sm:max-w-[900px]
           sm:translate-x-[-50%] sm:translate-y-[-50%]
           sm:rounded-lg sm:border sm:p-6
         "
       >
-        <DialogHeader>
+        <DialogHeader className="text-left">
           <DialogTitle>{title || ''}</DialogTitle>
           {description && <DialogDescription>{description}</DialogDescription>}
         </DialogHeader>
